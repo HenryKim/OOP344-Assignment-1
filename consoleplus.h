561464b6d92da990c4e3ed363da106fefca4de01
HernyKim <ex.hyungi93@gmail.com>
2015-06-22 17:46:08 -0400

    initial
    initial

---
+++ consoleplus.h
@@ -0,0 +1,14 @@
+/*
+OOP344 ASSIGNMENT 1 Line Editing Facility
+This Program has been written by
+Henry Hyungi Kim
+Student ID:025 741 125
+myseneca ID:hkim142
+OOP344 A Winter 2014
+To prof. Kamal-Al-Deen, Hasan
+*/
+namespace cio{
+// function definition delcariation
+void display(const char *str, int row, int col, int fieldLen);
+int edit(char *str, int row, int col, int fieldLength, int maxStrLength, bool* insertMode, int* strOffset, int* curPosition);
+}
